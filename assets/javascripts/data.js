0af059181e2dd08fd0e8ab0fd918d3bbdcd926ef
Quentin PLAUD <qplaud@gmail.com>
2021-01-21 14:16:56 +0100

    First commit with page canvas + old pattern of JS search

---
+++ assets/javascripts/data.js
@@ -0,0 +1,70 @@
+const data = {
+  "articles": [
+    {
+      "titre": "Devenir influenceur en 2020, tous nos conseils (la 4ème va vous étonner 😮)",
+      "tags": [
+        "twitch",
+        "influenceur"
+      ]
+    },
+    {
+      "titre": "Passer un confinement sans s'ennuyer",
+      "tags": [
+        "netflix",
+        "twitch",
+        "animalcrossing",
+      ] 
+    },
+    {
+      "titre": "Mes conseils 2020 de lecture",
+      "tags": [
+        "lecture",
+        "livres",
+        "bibliotheque"
+      ] 
+    },
+    {
+      "titre": "Devenir Front-End en 2020",
+      "tags": [
+        "frontend",
+        "javascript",
+        "react"
+      ] 
+    },
+    {
+      "titre": "Notre top des films à absolument voir !",
+      "tags": [
+        "straming",
+        "netflix"
+      ] 
+    },
+    {
+      "titre": "Les concepts ES6 à absolument connaître qui égayeront votre vie !",
+      "tags": [
+        "es6",
+        "javascript",
+        "frontend"
+      ] 
+    },
+    {
+      "titre": "Méditer pour ne plus stresser",
+      "tags": [
+        "lecture",
+        "conseils"
+      ] 
+    },
+    {
+      "titre": "Rien, effectivement, cela n'intéresse personne, mais ca s'affiche quand même",
+      "tags": [
+      ] 
+    },
+    {
+      "titre": "TOP 10 des pires copies de films (Feat Lama Faché)",
+      "tags": [
+          "film",
+          "streaming",
+          "netflix"
+      ] 
+    }
+  ]
+};
